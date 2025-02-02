6acaa5fc0f56898f760a3fedd6e41d9f80c7ab00
Evgeny Lugin <evgeny.lugin@tii.ae>
2025-02-02 11:22:35 +0400

    gst

--- cmd/downloader/main.go
+++ cmd/downloader/main.go
@@ -3,9 +3,8 @@ package main
 import (
 	"context"
 	"fmt"
-	"log/slog"
-
 	"github.com/chromedp/chromedp"
+	"log/slog"
 
 	dwl "mx_news_bot/internal/downloader"
 )
@@ -20,7 +19,7 @@ func main() {
 	defer cancel()
 
 	downloader := dwl.NewDownloader("https://results.supercrosslive.com/events/", "data/2025", &slog.Logger{})
-	eventList := []string{"Glendale"}
+	eventList := []string{"Anaheim 1"}
 
 	for _, eventName := range eventList {
 		if _, err := downloader.DownloadEventFiles(ctx, eventName); err != nil {
