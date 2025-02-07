a8712d4041b07f1273779c6044534a9323bf7ad9
Evgeny Lugin <evgeny.lugin@tii.ae>
2025-02-07 19:06:30 +0400

    Add TC standings

--- internal/formatter/formatter.go
+++ internal/formatter/formatter.go
@@ -185,7 +185,7 @@ func (f *TgFormatter) FormatTripleCrownResultTable(event models.Event, class str
 		EmojiBook, event.RoundNumber,
 		EmojiBook, class,
 	))
-	builder.WriteString("```")
+	builder.WriteString("```\n")
 	// Table header
 	builder.WriteString(fmt.Sprintf("%-3s | %-2s | %-15s | %-9s | %-2s | %-2s | %-2s | %-5s\n",
 		" Pos", "#", "Name", "Bike", "R1", "R2", "R3", "Total"))
@@ -196,7 +196,7 @@ func (f *TgFormatter) FormatTripleCrownResultTable(event models.Event, class str
 		builder.WriteString(fmt.Sprintf("%-2d | %-2s | %-15s | %-9s | %-2d | %-2d | %-2d | %-5d\n",
 			row.TotalPosition, row.RiderNumber, shortenName(row.Name), row.Bike, row.R1, row.R2, row.R3, row.TotalPoints))
 	}
-	builder.WriteString("```")
+	builder.WriteString("/n```")
 
 	return builder.String()
 }
