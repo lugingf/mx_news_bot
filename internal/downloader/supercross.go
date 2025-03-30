04eb24927c17485dc756d970c5cf2d05b147179c
Evgeny Lugin <evgeny.lugin@tii.ae>
2025-03-30 10:48:24 +0400

    fix

--- internal/downloader/supercross.go
+++ internal/downloader/supercross.go
@@ -108,7 +108,7 @@ type raceSet struct {
 }
 
 func (d *AMASupercross) getRaces(ctx context.Context, eventURL string) (raceSet, error) {
-	var links250, links250WH, links250EH, links250H1, links250H2 []string
+	var links250, links250M, links250EWS, links250WH, links250EH, links250H1, links250H2 []string
 	var links250R1, links250R2, links250R3 []string
 	var links450, links450H1, links450H2 []string
 	var links450R1, links450R2, links450R3 []string
@@ -116,11 +116,9 @@ func (d *AMASupercross) getRaces(ctx context.Context, eventURL string) (raceSet,
 	err := chromedp.Run(ctx,
 		chromedp.Navigate(eventURL),
 		chromedp.WaitVisible(`a`, chromedp.ByQuery), // Ensure the links are visible
-		chromedp.Evaluate(`Array.from(document.querySelectorAll('a'))
-  .filter(a => a.innerText === '250 Main Event')
-  .map(a => a.href)`, &links250),
-		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => a.textContent.match(/250 Main(?: Event)?/)).map(a => a.href)`, &links250),
-		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => a.textContent.match(/250 East\/West Showdown Main(?: Event)?/)).map(a => a.href)`, &links250),
+		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => a.innerText === '250 Main Event').map(a => a.href)`, &links250),
+		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => a.textContent.match(/250 Main(?: Event)?/)).map(a => a.href)`, &links250M),
+		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => a.textContent.match(/250 East\/West Showdown Main(?: Event)?/)).map(a => a.href)`, &links250EWS),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => /250 Heat (\#?1)/.test(a.textContent)).map(a => a.href)`, &links250H1),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => /250 West Heat/.test(a.textContent)).map(a => a.href)`, &links250WH),
 		chromedp.Evaluate(`Array.from(document.querySelectorAll('a')).filter(a => /250 East Heat/.test(a.textContent)).map(a => a.href)`, &links250EH),
@@ -142,7 +140,7 @@ func (d *AMASupercross) getRaces(ctx context.Context, eventURL string) (raceSet,
 
 	result := raceSet{
 		links: map[string][]string{
-			"250 Main_Event": links250,
+			"250 Main_Event": append(append(links250, links250M...), links250EWS...),
 			"250 Heat_1":     links250H1,
 			"250 Heat_2":     links250H2,
 			"250 West_Heat":  links250WH,
